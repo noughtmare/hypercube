30f77fb5e1723555b918cd11c700fb60b94c88fc
KReinders <ksreinders@gmail.com>
2017-04-13 18:23:46 +0200

    Face Lighting

--- hypercube.v.glsl
+++ hypercube.v.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoord;
+layout (location = 2) in float light;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,6 +12,6 @@ out vec2 TexCoord;
 
 void main(void) {
   gl_Position = projection * view * model * vec4(position, 1.0);
-  ourColor = vec3(1.0,1.0,1.0);
+  ourColor = vec3(light,light,light);
   TexCoord = texCoord;
 }
--- src/Faces.hs
+++ src/Faces.hs
@@ -2,59 +2,59 @@ module Faces where
 
 import Linear
 
-northFace, southFace, eastFace, westFace, topFace, bottomFace :: [(V3 Float, V2 Float)]
+northFace, southFace, eastFace, westFace, topFace, bottomFace :: [(V3 Float, V2 Float, Float)]
 
 northFace = 
-  [ (V3 1 1 1, V2 0 0) -- top    right
-  , (V3 0 1 1, V2 1 0) -- top    left
-  , (V3 0 0 1, V2 1 1) -- bottom left
-  , (V3 0 0 1, V2 1 1) -- bottom left
-  , (V3 1 0 1, V2 0 1) -- bottom right
-  , (V3 1 1 1, V2 0 0) -- top    right
+  [ (V3 1 1 1, V2 0 0, 0.7) -- top    right
+  , (V3 0 1 1, V2 1 0, 0.7) -- top    left
+  , (V3 0 0 1, V2 1 1, 0.7) -- bottom left
+  , (V3 0 0 1, V2 1 1, 0.7) -- bottom left
+  , (V3 1 0 1, V2 0 1, 0.7) -- bottom right
+  , (V3 1 1 1, V2 0 0, 0.7) -- top    right
   ]
 
 southFace = 
-  [ (V3 0 0 0, V2 1 1) -- bottom left
-  , (V3 0 1 0, V2 1 0) -- top    left
-  , (V3 1 1 0, V2 0 0) -- top    right
-  , (V3 1 1 0, V2 0 0) -- top    right
-  , (V3 1 0 0, V2 0 1) -- bottom right
-  , (V3 0 0 0, V2 1 1) -- bottom left
+  [ (V3 0 0 0, V2 1 1, 0.7) -- bottom left
+  , (V3 0 1 0, V2 1 0, 0.7) -- top    left
+  , (V3 1 1 0, V2 0 0, 0.7) -- top    right
+  , (V3 1 1 0, V2 0 0, 0.7) -- top    right
+  , (V3 1 0 0, V2 0 1, 0.7) -- bottom right
+  , (V3 0 0 0, V2 1 1, 0.7) -- bottom left
   ]
 
 eastFace =
-  [ (V3 1 1 0, V2 0 0) -- top    right
-  , (V3 1 1 1, V2 1 0) -- top    left
-  , (V3 1 0 1, V2 1 1) -- bottom left
-  , (V3 1 0 1, V2 1 1) -- bottom left
-  , (V3 1 0 0, V2 0 1) -- bottom right
-  , (V3 1 1 0, V2 0 0) -- top    right
+  [ (V3 1 1 0, V2 0 0, 0.6) -- top    right
+  , (V3 1 1 1, V2 1 0, 0.6) -- top    left
+  , (V3 1 0 1, V2 1 1, 0.6) -- bottom left
+  , (V3 1 0 1, V2 1 1, 0.6) -- bottom left
+  , (V3 1 0 0, V2 0 1, 0.6) -- bottom right
+  , (V3 1 1 0, V2 0 0, 0.6) -- top    right
   ]
   
 westFace = 
-  [ (V3 0 0 1, V2 1 1) -- bottom left
-  , (V3 0 1 1, V2 1 0) -- top    left
-  , (V3 0 1 0, V2 0 0) -- top    right
-  , (V3 0 1 0, V2 0 0) -- top    right
-  , (V3 0 0 0, V2 0 1) -- bottom right
-  , (V3 0 0 1, V2 1 1) -- bottom left
+  [ (V3 0 0 1, V2 1 1, 0.6) -- bottom left
+  , (V3 0 1 1, V2 1 0, 0.6) -- top    left
+  , (V3 0 1 0, V2 0 0, 0.6) -- top    right
+  , (V3 0 1 0, V2 0 0, 0.6) -- top    right
+  , (V3 0 0 0, V2 0 1, 0.6) -- bottom right
+  , (V3 0 0 1, V2 1 1, 0.6) -- bottom left
   ]
 
 topFace =
-  [ (V3 0 1 0, V2 1 1) -- bottom left
-  , (V3 0 1 1, V2 1 0) -- top    left
-  , (V3 1 1 1, V2 0 0) -- top    right
-  , (V3 1 1 1, V2 0 0) -- top    right
-  , (V3 1 1 0, V2 0 1) -- bottom right
-  , (V3 0 1 0, V2 1 1) -- bottom left
+  [ (V3 0 1 0, V2 1 1, 0.8) -- bottom left
+  , (V3 0 1 1, V2 1 0, 0.8) -- top    left
+  , (V3 1 1 1, V2 0 0, 0.8) -- top    right
+  , (V3 1 1 1, V2 0 0, 0.8) -- top    right
+  , (V3 1 1 0, V2 0 1, 0.8) -- bottom right
+  , (V3 0 1 0, V2 1 1, 0.8) -- bottom left
   ]
 
 bottomFace =
-  [ (V3 1 0 1, V2 0 0) -- top    right
-  , (V3 0 0 1, V2 1 0) -- top    left
-  , (V3 0 0 0, V2 1 1) -- bottom left
-  , (V3 0 0 0, V2 1 1) -- bottom left
-  , (V3 1 0 0, V2 0 1) -- bottom right
-  , (V3 1 0 1, V2 0 0) -- top    right
+  [ (V3 1 0 1, V2 0 0, 0.5) -- top    right
+  , (V3 0 0 1, V2 1 0, 0.5) -- top    left
+  , (V3 0 0 0, V2 1 1, 0.5) -- bottom left
+  , (V3 0 0 0, V2 1 1, 0.5) -- bottom left
+  , (V3 1 0 0, V2 0 1, 0.5) -- bottom right
+  , (V3 1 0 1, V2 0 0, 0.5) -- top    right
   ]
 
