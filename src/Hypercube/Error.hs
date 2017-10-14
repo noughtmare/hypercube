4767f8572c33d350b5ddfc0e00855fd083b021ed
Florent Lioult <florent@hihan.org>
2017-10-14 10:52:35 +0200

    Removed unnecessary qualifier.

--- src/Hypercube/Error.hs
+++ src/Hypercube/Error.hs
@@ -3,24 +3,24 @@ module Hypercube.Error (
 ) where
 
 import Control.Monad
-import qualified Graphics.GL as Raw
+import Graphics.GL
 
-getErrors :: IO [Raw.GLuint]
+getErrors :: IO [GLuint]
 getErrors = do
-    error <- Raw.glGetError
-    if error == Raw.GL_NO_ERROR
+    error <- glGetError
+    if error == GL_NO_ERROR
         then return []
         else (error:) <$> getErrors
 
-showError :: Raw.GLuint-> String
+showError :: GLuint-> String
 showError error = case error of
-    Raw.GL_INVALID_ENUM -> "GL_INVALID_ENUM"
-    Raw.GL_INVALID_VALUE -> "GL_INVALID_VALUE"
-    Raw.GL_INVALID_OPERATION -> "GL_INVALID_OPERATION"
-    Raw.GL_INVALID_FRAMEBUFFER_OPERATION -> "GL_INVALID_FRAMEBUFFER_OPERATION"
-    Raw.GL_OUT_OF_MEMORY -> "GL_OUT_OF_MEMORY"
-    Raw.GL_STACK_UNDERFLOW -> "GL_STACK_UNDERFLOW"
-    Raw.GL_STACK_OVERFLOW -> "GL_STACK_OVERFLOW"
+    GL_INVALID_ENUM -> "GL_INVALID_ENUM"
+    GL_INVALID_VALUE -> "GL_INVALID_VALUE"
+    GL_INVALID_OPERATION -> "GL_INVALID_OPERATION"
+    GL_INVALID_FRAMEBUFFER_OPERATION -> "GL_INVALID_FRAMEBUFFER_OPERATION"
+    GL_OUT_OF_MEMORY -> "GL_OUT_OF_MEMORY"
+    GL_STACK_UNDERFLOW -> "GL_STACK_UNDERFLOW"
+    GL_STACK_OVERFLOW -> "GL_STACK_OVERFLOW"
     x -> "GL Error " ++ show x
 
 printErrors :: String -> IO ()
